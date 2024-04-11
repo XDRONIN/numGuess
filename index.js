7e5d17488fcb44a33e39b1c9289dc32879e73bfe
XDRONIN <akashprakashmattathil@gmail.com>
2024-04-11 22:29:31 +0530

    The game mechanics works!

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+let minnum = 1;
+let maxnum = 100;
+let answer = Math.floor(Math.random() * (maxnum - minnum + 1) * minnum);
+let running = true;
+while (running) {
+  let guess = window.prompt("Enter a Number between 1 and 100");
+  guess = Number(guess);
+  if (guess < answer) {
+    window.alert("Too Low");
+  } else if (guess > answer) {
+    window.alert("Too High");
+  } else {
+    window.alert("You guessed right");
+    running = false;
+  }
+}
